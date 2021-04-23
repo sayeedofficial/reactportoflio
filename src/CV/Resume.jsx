b084dad1f1173bd79848e30ddce20d3767ec0b89
Mohammed Sayeed <mdsayeed6220@gmail.com>
2021-04-23 12:20:47 +0530

    Altered Cv Route

--- src/CV/Resume.jsx
+++ src/CV/Resume.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import "./Resume.css";
-import cv from "./MyCv.pdf";
+
 const Resume = () => {
   return (
     <div className="resume">
-      <a  href={cv}>
-        <button className="custom-btn btn-1">Click Here</button>
-      </a>
+      <h1>Commin Soon...</h1>
     </div>
   );
 };
