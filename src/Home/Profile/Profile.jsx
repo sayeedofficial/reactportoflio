575dba0dde3070691a7790134235b17236ff7636
Mohammed Sayeed <sayeedofficial6220@gmail.com>
2021-05-06 23:48:49 +0530

    Fixed Profile Links

--- src/Home/Profile/Profile.jsx
+++ src/Home/Profile/Profile.jsx
@@ -3,20 +3,19 @@ import "./Profile.css";
 import portfolio11 from "../../Images/portfolio11.jpg";
 const Profile = () => {
   return (
-      <div className="card">
-          <h1>Profile</h1>
+    <div className="card">
+      <h1>Profile</h1>
       <img className="my-img" src={portfolio11} alt="" />
       <h1>Mohammed Sayeed</h1>
       <p className="title">Full Stack Developer</p>
-      
       <a
         className="profile-link"
         href="https://www.linkedin.com/in/sayeedofficial/"
       >
-        <i className="fab fa-github-square"> </i>
+        <i className="fab fa-linkedin"> </i>
       </a>
       <a className="profile-link" href="https://github.com/sayeedofficial">
-        <i className="fab fa-linkedin"> </i>
+        <i className="fab fa-github-square"> </i>
       </a>
     </div>
   );
