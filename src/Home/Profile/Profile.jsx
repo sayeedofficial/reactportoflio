006b841665064e61238b637bce20191603958333
Mohammed Sayeed <sayeedofficial6220@gmail.com>
2021-05-15 23:15:09 +0530

    Added new profile pic

--- src/Home/Profile/Profile.jsx
+++ src/Home/Profile/Profile.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./Profile.css";
-import portfolio11 from "../../Images/portfolio11.jpg";
+import micpic from "../../Images/micpic.png";
 const Profile = () => {
   return (
     <div className="card">
       <h1>Profile</h1>
-      <img className="my-img" src={portfolio11} alt="" />
+      <img className="my-img" src={micpic} alt="" />
       <h1>Mohammed Sayeed</h1>
       <p className="title">Full Stack Developer</p>
       <a className="profile-link" href="https://github.com/sayeedofficial">
